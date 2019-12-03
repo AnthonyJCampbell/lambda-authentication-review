e9b729161fb9852c4aa6f1079dc5c7e9c6b5ed05
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-12-03 15:52:30 +0100

    Playing around with hashing passwords on endpoints

--- day1/guided/users/users-router.js
+++ day1/guided/users/users-router.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs')
 const Users = require('./users-model.js');
 
 router.get('/', (req, res) => {
@@ -9,5 +9,29 @@ router.get('/', (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 12)
+
+  user.password = hash;
+  
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+})
+
+router.get('/hash', (req, res) => {
+  const password = req.headers.authorization;
+
+  const hash = bcrypt.hashSync(password, 12)
+
+  res.status(200).json({hash})
+})
 
 module.exports = router;
